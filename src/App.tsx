99eeaae570411663184474fb4dd6bf14766c8484
Renaud Racinet <renaud.racinet@estiam.com>
2020-02-02 10:40:13 +0100

    feat(app.tsx): Beginning of coding

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table} from 'react-bootstrap';
 
-const App = () => {
+const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="container">
+        <Table striped bordered hover responsive>
+          <thead>
+          <tr>
+            <th>
+              test
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                test
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
