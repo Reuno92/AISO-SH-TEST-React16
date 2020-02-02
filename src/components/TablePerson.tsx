bab5fb27621060c8e12b8e14f3abe2f68a72292f
Renaud Racinet <renaud.racinet@estiam.com>
2020-02-02 12:44:40 +0100

    feat(TablePerson): humanize date
    Add MomentJS dependency
    Add format date for human

--- src/components/TablePerson.tsx
+++ src/components/TablePerson.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {Button, Table} from 'react-bootstrap';
 import {Person} from '../model/Person';
+import moment from 'moment';
 
 type TablePersonProps = {
 	data: Array<Person>
@@ -8,41 +9,39 @@ type TablePersonProps = {
 
 const TablePerson: FC<TablePersonProps> = ({data}: TablePersonProps) => {
 
-	console.log(data);
-
-	return(
+	return (
 		<React.Fragment>
-		<Button variant="success">Create a new arrival</Button>
-		<Table striped bordered hover responsive>
-			<thead>
-			<tr>
-				<th>#id</th>
-				<th>First name</th>
-				<th>Last name</th>
-				<th>Job</th>
-				<th>Arrival date</th>
-				<th>Budget</th>
-				<th>Action</th>
-			</tr>
-			</thead>
-			<tbody>
-			{
-				data !== [] && data.map( person => (
-					<tr key={person.id}>
-						<td>{person.id}</td>
-						<td>{person.firstName}</td>
-						<td>{person.lastName}</td>
-						<td>{person.job}</td>
-						<td>{person.arrivalDate}</td>
-						<td>{person.budget}</td>
-						<td><Button variant="primary">See</Button></td>
-					</tr>
-				))
-			}
-			</tbody>
-		</Table>
+			<Button variant="success">Create a new arrival</Button>
+			<Table striped bordered hover responsive>
+				<thead>
+				<tr>
+					<th>#id</th>
+					<th>First name</th>
+					<th>Last name</th>
+					<th>Job</th>
+					<th>Arrival date</th>
+					<th>Budget</th>
+					<th>Action</th>
+				</tr>
+				</thead>
+				<tbody>
+				{
+					data !== [] && data.map(person => (
+						<tr key={person.id}>
+							<td>{person.id}</td>
+							<td>{person.firstName}</td>
+							<td>{person.lastName}</td>
+							<td>{person.job}</td>
+							<td>{moment(person.arrivalDate).format('LL')}</td>
+							<td>{person.budget}</td>
+							<td><Button variant="primary">See</Button></td>
+						</tr>
+					))
+				}
+				</tbody>
+			</Table>
 		</React.Fragment>
-	)
+	);
 };
 
 export default TablePerson;
